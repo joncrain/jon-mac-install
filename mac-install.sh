2bb5242f409b7a2a2c85b88535e2488be9311a91
joncrain <joncrain@users.noreply.github.com>
2016-09-09 16:33:54 -0400

    updating home-brew link

--- mac-install.sh
+++ mac-install.sh
@@ -7,7 +7,7 @@ which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
     # https://github.com/mxcl/homebrew/wiki/installation
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
     brew update
 fi
