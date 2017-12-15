1626cb23feef98e7f21995bd7b17223491fb72e4
Jon Crain <joncrain@users.noreply.github.com>
2017-12-15 11:44:59 -0500

    updating for 2017

--- mac-install.sh
+++ mac-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script author: Jon Crain
-# Version 1.2 || 2016-9-9
+# Version 1.3 || 2017-12-15
 # This script is not meant to be called as part of the deployment/imaging
 # It's simply for when I get a new computer or loaner or something...
 
@@ -33,7 +33,8 @@ defaults write com.apple.driver.applebluetoothmultitouch.trackpad.plist Clicking
 #    pip install --user git+git://github.com/Lokaltog/powerline
 #    brew install vim --with-python --with-ruby --with-perl
 #    pip install --user git+git://github.com/Lokaltog/powerline
-
+easy_install pip
+pip install requests
 
 # Install Cask
 brew tap caskroom/cask
@@ -46,9 +47,10 @@ brew cask install 1password
 brew cask install appcleaner
 brew cask install atom
 brew cask install autodmg
+brew cask install autopkgr
+brew cask install bbedit
 brew cask install caffeine
-brew cask install createuserpkg
-brew cask install cyberduck
+brew cask install docker
 brew cask install dropbox
 brew cask install filezilla
 brew cask install firefox
@@ -63,22 +65,30 @@ brew cask install iterm2
 brew cask install java
 brew cask install kaleidoscope
 brew cask install karabiner
+brew cask install kitematic
 brew cask install macvim
 brew cask install navicat-premium
+brew cask install nomad
+brew cask install onedrive
+brew cask install opera
 brew cask install nvalt
 brew cask install postman
-brew cask install quitter
-brew cask install rescuetime
+brew cask install royal-tsx
+brew cask install sdformatter
 brew cask install skype
 brew cask install slack
 brew cask install spectacle
 brew cask install spotify
 brew cask install steam
+brew cask install suspicious-package
 brew cask install textexpander
-brew cask install textwrangler
 brew cask install vagrant
+brew cask install veertu
 brew cask install virtualbox
 brew cask install vlc
+brew cask install vmware-horizon-client
+brew cask install vmware-remote-console
+brew cask install vnc-viewer
 
 echo "Cleaning up ..."
 brew cleanup
@@ -88,3 +98,9 @@ mkdir ~/Projects
 cd ~/Projects
 # inception
 git clone https://github.com/joncrain/jon-mac-install.git
+git clone https://code.cmich.edu/mac/deploystudio.git
+git clone https://code.cmich.edu/mac/profiles.git
+git clone https://code.cmich.edu/mac/packages.git
+git clone https://code.cmich.edu/mac/munki-repo.git
+git clone https://code.cmich.edu/mac/autopkgr.git
+
